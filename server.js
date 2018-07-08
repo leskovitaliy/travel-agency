51fd0aad1461ca7a3006b2d42e6fc787cdfa9403
leskovitaliy <lesko.vitaliy90@gmail.com>
2018-07-08 21:42:38 +0300

    change newsletter pages, change api for sent to form

--- server.js
+++ server.js
@@ -5,7 +5,16 @@ const app = express();
 
 // set up handlebars view engine
 const handlebars = require('express-handlebars')
-    .create({ defaultLayout:'main' });
+    .create({
+        defaultLayout: 'main',
+        helpers: {
+            section: function(name, options){
+                if(!this._sections) this._sections = {};
+                this._sections[name] = options.fn(this);
+                return null;
+            }
+        }
+    });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
@@ -16,7 +25,46 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
+
+function getWeatherData(){
+    return {
+        locations: [
+            {
+                name: 'Портленд',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+                weather: 'Сплошная облачность ',
+                temp: '54.1 F (12.3 C)',
+            },
+            {
+                name: 'Бенд',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+                weather: 'Малооблачно',
+                temp: '55.0 F (12.8 C)',
+            },
+            {
+                name: 'Манзанита',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+                weather: 'Небольшой дождь',
+                temp: '55.0 F (12.8 C)',
+            }
+        ]
+    }
+}
+
+// middleware to add weather data to context
+app.use((req, res, next) => {
+    if (!res.locals.partials) {
+        res.locals.partials = {};
+    }
+    res.locals.partials.weatherContext = getWeatherData();
+    next();
+});
+
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -39,6 +87,24 @@ app.get('/tours/request-group-rate', (req, res) => {
     res.render('tours/request-group-rate');
 });
 
+app.get('/thank-you', function(req, res){
+    res.render('thank-you');
+});
+
+app.get('/newsletter', (req, res) => {
+// заполняем фиктивное значение
+    res.render('newsletter', { csrf: 'CSRF token goes here' });
+});
+app.post('/process', function(req, res){
+    if(req.xhr || req.accepts('json,html' )==='json' ){
+// если здесь есть ошибка, то мы должны отправить { error: 'описание ошибки' }
+        res.send({ success: true });
+    } else {
+// если бы была ошибка, нам нужно было бы перенаправлять на страницу ошибки
+        res.redirect(303, '/thank-you' );
+    }
+});
+
 // page 404
 app.use((req, res) => {
     res.status(404);
@@ -47,9 +113,9 @@ app.use((req, res) => {
 
 // page 500
 app.use((err, req, res, next) => {
-   console.error(err.stack);
-   res.status(500);
-   res.render('500');
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
 });
 
 app.listen(app.get('port'), () => {
